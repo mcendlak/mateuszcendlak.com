82dc82a8aaa7d0bc2f490fe971bb44c04a8dad6b
Mateusz Cendlak <mcendlak@gmail.com>
2021-05-28 18:49:57 +0200

    Aminate hamburger

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+// ===============
+// DOM ELEMENTS
+// ===============
+const hamburger = document.querySelector('.hamburger');
 const scrollToTopBtn = document.querySelector('.aside__navigation');
+
+// ===============
+// EVENT LISTENERS
+// ===============
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('h-active');
+});
 
 window.addEventListener('scroll', () => {
   window.scrollY > window.innerHeight / 2 ?
   scrollToTopBtn.classList.add('active') :
   scrollToTopBtn.classList.remove('active');
-})
+});
