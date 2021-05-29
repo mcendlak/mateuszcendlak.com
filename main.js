d44858938dd19790d3fdb88345ba8b9d8c0778b5
Mateusz Cendlak <mcendlak@gmail.com>
2021-05-29 12:48:14 +0200

    Improve mobile nav functionality and add gitignore

--- main.js
+++ main.js
@@ -3,15 +3,24 @@
 // ===============
 const hamburger = document.querySelector('.hamburger');
 const mainMenu = document.querySelector('.main-menu');
+const mainMenuList = document.querySelector('.main-menu__list');
 const scrollToTopBtn = document.querySelector('.aside__navigation');
+
+
+// ===============
+// FUNCTIONS
+// ===============
+const mobileMenuClassToggleHandler = () => {
+  hamburger.classList.toggle('h-active');
+  mainMenu.classList.toggle('visible');
+};
+
 
 // ===============
 // EVENT LISTENERS
 // ===============
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('h-active');
-  mainMenu.classList.toggle('visible');
-});
+hamburger.addEventListener('click', mobileMenuClassToggleHandler);
+mainMenuList.addEventListener('click', mobileMenuClassToggleHandler);
 
 window.addEventListener('scroll', () => {
   window.scrollY > window.innerHeight / 2 ?
