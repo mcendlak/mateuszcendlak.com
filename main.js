08e52b1e453c515d1f85e0321a388ed5b4868d75
Mateusz Cendlak <mcendlak@gmail.com>
2021-06-05 06:16:05 +0200

    Improve background solution

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const lastSection = document.getElementById('contact');
 const mobileMenuClassToggleHandler = () => {
   hamburger.classList.toggle('h-active');
   mainMenu.classList.toggle('visible');
+  document.body.classList.toggle('fixed');
 };
 
 
